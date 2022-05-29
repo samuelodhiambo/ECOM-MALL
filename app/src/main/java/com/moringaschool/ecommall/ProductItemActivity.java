212447933b86f42ee86ac10b4da2409f81ee3f11
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-05-30 00:48:16 +0300

    Set text for product name

--- app/src/main/java/com/moringaschool/ecommall/ProductItemActivity.java
+++ app/src/main/java/com/moringaschool/ecommall/ProductItemActivity.java
@@ -2,9 +2,11 @@ package com.moringaschool.ecommall;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -12,6 +14,8 @@ import butterknife.ButterKnife;
 public class ProductItemActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.backArrow)
     ImageView backArrow;
+    @BindView(R.id.itemName)
+    TextView itemName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,10 @@ public class ProductItemActivity extends AppCompatActivity implements View.OnCli
         ButterKnife.bind(this);
 
         backArrow.setOnClickListener(this);
+
+        Intent intent = getIntent();
+        String item = intent.getStringExtra("product");
+        itemName.setText(item);
     }
 
     @Override
