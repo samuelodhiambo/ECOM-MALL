3ad23285009eac2e997a92b050d72754fe3f0f17
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-05-29 23:40:04 +0300

    Create back button

--- app/src/main/java/com/moringaschool/ecommall/ProductItemActivity.java
+++ app/src/main/java/com/moringaschool/ecommall/ProductItemActivity.java
@@ -3,12 +3,29 @@ package com.moringaschool.ecommall;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class ProductItemActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ProductItemActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.backArrow)
+    ImageView backArrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_item);
+        ButterKnife.bind(this);
+
+        backArrow.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == backArrow) {
+            this.finish();
+        }
     }
 }
