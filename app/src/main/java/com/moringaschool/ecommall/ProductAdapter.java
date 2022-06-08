3e5804562190ea3c1bc5531cde85767bf347f714
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-06-08 08:35:37 +0300

    Optimize product adapter to use data from Models

--- app/src/main/java/com/moringaschool/ecommall/ProductAdapter.java
+++ app/src/main/java/com/moringaschool/ecommall/ProductAdapter.java
@@ -1,6 +1,8 @@
 package com.moringaschool.ecommall;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,24 +10,33 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.moringaschool.ecommall.Models.Products.Datum;
+import com.squareup.picasso.Picasso;
+
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductAdapter extends BaseAdapter {
     private Context mContext;
     private LayoutInflater inflater;
-    private String[] products;
+    private List<Datum> products;
 
-    public ProductAdapter(Context mContext, String[] products) {
+    public ProductAdapter(Context mContext, List<Datum> products) {
         this.mContext = mContext;
         this.products = products;
     }
 
     @Override
     public int getCount() {
-        return products.length;
+        return products.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return products[position];
+        return products.get(position);
     }
 
     @Override
@@ -41,11 +52,16 @@ public class ProductAdapter extends BaseAdapter {
         if (convertView == null) {
             convertView = inflater.inflate(R.layout.product_item, null);
         }
-//        ImageView productImage = convertView.findViewById(R.id.productImage);
+        ConstraintLayout constraintLayout = convertView.findViewById(R.id.constraintLayout3);
+        int width = Resources.getSystem().getDisplayMetrics().widthPixels;
+        int height = Resources.getSystem().getDisplayMetrics().heightPixels;
+        constraintLayout.getLayoutParams().width = (width/2)-16;
+        constraintLayout.getLayoutParams().height = height/3;
+        ImageView productImage = convertView.findViewById(R.id.productImage);
         TextView productName = convertView.findViewById(R.id.productName);
 
-//        productImage.setImageResource();
-        productName.setText(products[position]);
+        Picasso.get().load(products.get(position).getImage().getUrl()).into(productImage);
+        productName.setText(products.get(position).getName());
         return convertView;
     }
 }
