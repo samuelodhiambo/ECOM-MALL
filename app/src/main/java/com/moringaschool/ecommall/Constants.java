56ef70b279719d0adf0715eb64d4ba73b5c9155a
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-06-08 08:31:07 +0300

    Add publicly accessible constants for API Key and URL

---
+++ app/src/main/java/com/moringaschool/ecommall/Constants.java
@@ -0,0 +1,6 @@
+package com.moringaschool.ecommall;
+
+public class Constants {
+    public static String BASE_URL = "https://api.chec.io/v1/";
+    public static String API_KEY = BuildConfig.COMMERCEJS_API_KEY;
+}
