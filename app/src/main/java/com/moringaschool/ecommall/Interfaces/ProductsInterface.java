184de6d75bdecf37944877de5392f6a5e030f6cf
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-06-15 13:46:05 +0300

    Add retrofit interface to query API

--- app/src/main/java/com/moringaschool/ecommall/Interfaces/ProductsInterface.java
+++ app/src/main/java/com/moringaschool/ecommall/Interfaces/ProductsInterface.java
@@ -1,11 +1,24 @@
 package com.moringaschool.ecommall.Interfaces;
 
+import com.moringaschool.ecommall.Models.Categories.Category;
 import com.moringaschool.ecommall.Models.Products.Product;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface ProductsInterface {
     @GET("products")
     Call<Product> getAllProducts();
+
+    @GET("products")
+    Call<Product> searchProducts(@Query("query") String query);
+
+    @GET("products")
+    Call<Product> getProductsByCategory(@Query("category_id") String category_id);
+
+    @GET("categories")
+    Call<Category> getAllCategories();
 }
