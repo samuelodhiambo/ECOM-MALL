1a6863b9bc4c0b047cec23096304f57bd76f52b5
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-06-15 13:55:41 +0300

    Import layout Object R

--- app/src/main/java/com/moringaschool/ecommall/ui/SettingsFragment.java
+++ app/src/main/java/com/moringaschool/ecommall/ui/SettingsFragment.java
@@ -1,4 +1,4 @@
-package com.moringaschool.ecommall;
+package com.moringaschool.ecommall.ui;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.moringaschool.ecommall.R;
 
 /**
  * A simple {@link Fragment} subclass.
