e0b481c4bd7bfef08b40b36ada1bd7b969ae3a83
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-06-15 13:42:43 +0300

    Add splash activity for splash screen

---
+++ app/src/main/java/com/moringaschool/ecommall/ui/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.moringaschool.ecommall.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.moringaschool.ecommall.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // remove title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
