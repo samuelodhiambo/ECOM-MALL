e5f8c7c00fd48da6d0a1115ceb12a10ed310207a
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-06-22 22:46:09 +0300

    Add logic to handle buttons click.

--- app/src/main/java/com/moringaschool/ecommall/ui/SettingsFragment.java
+++ app/src/main/java/com/moringaschool/ecommall/ui/SettingsFragment.java
@@ -1,21 +1,40 @@
 package com.moringaschool.ecommall.ui;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.moringaschool.ecommall.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link SettingsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements View.OnClickListener {
+    @BindView(R.id.signUpButton)
+    Button signUpButton;
+    @BindView(R.id.logInButton)
+    Button logInButton;
+    @BindView(R.id.logoutButton)
+    Button logoutButton;
+
+    private FirebaseAuth mAuth;
+    FirebaseUser currentUser;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -51,6 +70,8 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -60,7 +81,43 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        ButterKnife.bind(this, view);
+
+        signUpButton.setOnClickListener(this);
+        logInButton.setOnClickListener(this);
+        logoutButton.setVisibility(View.INVISIBLE);
+        logoutButton.setOnClickListener(this);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        currentUser = mAuth.getCurrentUser();
+        if(currentUser != null && currentUser.getEmail() != null){
+            logInButton.setVisibility(View.INVISIBLE);
+            signUpButton.setVisibility(View.INVISIBLE);
+            logoutButton.setVisibility(View.VISIBLE);
+            logoutButton.setText("LogOut (" + currentUser.getEmail() + ")");
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == signUpButton | v == logInButton){
+            Intent intent = new Intent(getContext(), AuthenticationActivity.class);
+            startActivity(intent);
+        }
+        if (v == logoutButton){
+            FirebaseAuth.getInstance().signOut();
+            FragmentManager fm = getParentFragmentManager();
+            FragmentTransaction fragmentTransaction = fm.beginTransaction();
+            fragmentTransaction.replace(R.id.main_frame, new SettingsFragment());
+            fragmentTransaction.commit();
+        }
     }
 }
