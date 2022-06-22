7d52646d8d09e853d2217b43639e6413aa70a4c8
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-06-22 22:32:29 +0300

    Firebase validate anonymous user

--- app/src/main/java/com/moringaschool/ecommall/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/ecommall/ui/MainActivity.java
@@ -7,15 +7,26 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.moringaschool.ecommall.R;
 import com.moringaschool.ecommall.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
 
+    private static final String TAG = MainActivity.class.toString();
     ActivityMainBinding binding;
+
+    private FirebaseAuth mAuth;
+    FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +37,33 @@ public class MainActivity extends AppCompatActivity implements NavigationBarView
 
         binding.bottomNavigationView.setOnItemSelectedListener(this);
 
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        currentUser = mAuth.getCurrentUser();
+        if (currentUser == null || currentUser.getEmail() == null){
+            mAuth.signInAnonymously()
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "signInAnonymously:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                currentUser = user;
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInAnonymously:failure", task.getException());
+                                Toast.makeText(getBaseContext(), "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
     }
 
 
