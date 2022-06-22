97e5bb97e0a371a50c6a384894695d13437e5a27
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-06-22 22:23:28 +0300

    Optimize bindCartProduct method

--- app/src/main/java/com/moringaschool/ecommall/Adapters/FirebaseCartViewHolder.java
+++ app/src/main/java/com/moringaschool/ecommall/Adapters/FirebaseCartViewHolder.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -19,9 +21,14 @@ import com.squareup.picasso.Picasso;
 
 import org.jsoup.Jsoup;
 
+import java.util.Objects;
+
 public class FirebaseCartViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     View view;
     Context mContext;
+
+    private FirebaseAuth mAuth;
+    FirebaseUser currentUser;
 
     private final FirebaseDatabase database = FirebaseDatabase.getInstance();
     private final DatabaseReference ref = database.getReferenceFromUrl("https://ecom-mall-default-rtdb.firebaseio.com/");
@@ -32,21 +39,33 @@ public class FirebaseCartViewHolder extends RecyclerView.ViewHolder implements V
         view = itemView;
         mContext = itemView.getContext();
         itemView.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
     }
 
-    public void bindCartProduct(Datum cartProduct){
-        ImageView cartImage = view.findViewById(R.id.cartImage);
-        TextView cartName, cartDesc, cartPrice;
-        cartName = view.findViewById(R.id.cartName);
-        cartDesc = view.findViewById(R.id.cartDesc);
-        cartPrice = view.findViewById(R.id.cartPrice);
+    public void bindCartProduct(@NonNull Datum cartProduct){
+        currentUser = mAuth.getCurrentUser();
+        if (cartProduct.getName() != null && Objects.equals(cartProduct.getUid(), currentUser.getUid())){
+            ImageView cartImage = view.findViewById(R.id.cartImage);
+            TextView cartName, cartDesc, cartPrice;
+            cartName = view.findViewById(R.id.cartName);
+            cartDesc = view.findViewById(R.id.cartDesc);
+            cartPrice = view.findViewById(R.id.cartPrice);
 
-        Log.e("cartProd", cartProduct.toString());
+            Log.e("cartProd", cartProduct.toString());
 
-        Picasso.get().load(cartProduct.getImage().getUrl()).into(cartImage);
-        cartName.setText(cartProduct.getName());
-        cartDesc.setText(Jsoup.parse(cartProduct.getDescription()).text());
-        cartPrice.setText(cartProduct.getPrice().getFormattedWithSymbol());
+            Picasso.get().load(cartProduct.getImage().getUrl()).into(cartImage);
+            cartName.setText(cartProduct.getName());
+            cartDesc.setText(Jsoup.parse(cartProduct.getDescription()).text());
+            cartPrice.setText(cartProduct.getPrice().getFormattedWithSymbol());
+        } else {
+            view.findViewById(R.id.cartImage).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.cartName).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.cartDesc).setVisibility(View.INVISIBLE);
+            view.findViewById(R.id.cartPrice).setVisibility(View.INVISIBLE);
+            view.getLayoutParams().height = 0;
+            view.setVisibility(View.GONE);
+        }
+
     }
 
     @Override
